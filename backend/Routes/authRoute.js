3083ba4dd47ac1c425b1abf63e0d3fd63d898656
yuktaba.gohil <yuktaba.gohil@letsnurture.co.in>
2022-12-06 12:36:18 -0500

    validations signup and signin

--- backend/Routes/authRoute.js
+++ backend/Routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {userSignUpValidator, hospitalSignUpValidator, adminSignUpValidator, passwordResetValidator} =  require('../Validators/index')
+const {userSignUpValidator, hospitalSignUpValidator, adminSignUpValidator, userSignInValidator, hospitalSignInValidator, adminSignInValidator, passwordResetValidator} =  require('../Validators/index')
 const {signup, signin, signout, hospitalSignup, hospitalSignin, adminSignup, adminSignin, forgotPassword,resetPassword} = require('../Controllers/authController')
 
 router.put("/forgot-password", forgotPassword);
@@ -11,9 +11,9 @@ router.put("/reset-password", passwordResetValidator, resetPassword);
 router.post('/signup', userSignUpValidator, signup);
 router.post('/hospital-signup', hospitalSignUpValidator, hospitalSignup);
 router.post('/admin-signup', adminSignUpValidator, adminSignup);
-router.post('/signin', signin);
-router.post('/hospital-signin', hospitalSignin);
-router.post('/admin-signin', adminSignin);
+router.post('/signin', userSignInValidator, signin);
+router.post('/hospital-signin', hospitalSignInValidator,hospitalSignin);
+router.post('/admin-signin', adminSignInValidator,adminSignin);
 router.get('/signout', signout);
 
 module.exports = router;
--- backend/Validators/index.js
+++ backend/Validators/index.js
@@ -7,8 +7,8 @@ exports.userSignUpValidator = (req, res, next) => {
     })
     .withMessage("Name must contain at least 2 characters")
     //email is not null, valid and normalized with regular expression
-    req.check('email', 'Email is required')
-    .notEmpty()
+    req.check('email', "Email is required").notEmpty();
+    req.check('email', 'Email must be between 3 to 32 characters')
     .matches(/.+@.+\..+/)
     .withMessage('Email must contain @')
     .isLength({
@@ -17,10 +17,10 @@ exports.userSignUpValidator = (req, res, next) => {
     //check for password
     req.check('password', "Password is required").notEmpty();
     req.check('password')
-    .isLength({min:5})
-    .withMessage("Password must contain at least 5 characters")
-    .matches(/\d/)
-    .withMessage('Password must contain a number')
+    .isLength({min:8})
+    .withMessage("Password must contain at least 8 characters")
+    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)
+    .withMessage('Password must contain a number and a special character')
     //check for errors:
     const errors = req.validationErrors()
     //map throught the errors:
@@ -40,20 +40,21 @@ exports.hospitalSignUpValidator = (req, res, next) => {
         min: 2, max: 200
     })
     .withMessage("Name must contain at least 2 characters")
-     //address
-     req.check('address', "Address is required")
-     .notEmpty()
-     .isLength({
-         min: 2, max: 200
-     })
-     .withMessage("Name must contain at least 2 characters")
     //email is not null, valid and normalized with regular expression
+    req.check('hospitalEmail', "Email is required").notEmpty();
     req.check('hospitalEmail', 'Hospital Email must be between 3 to 32 characters')
     .matches(/.+@.+\..+/)
     .withMessage('Email must contain @')
     .isLength({
         min: 4, max: 2000
     })
+    //address
+    req.check('address', "Address is required")
+    .notEmpty()
+    .isLength({
+        min: 2, max: 200
+    })
+    .withMessage("Name must contain at least 2 characters")
     //person Name
     req.check('personName', "Person Name is required")
     .notEmpty()
@@ -73,8 +74,8 @@ exports.hospitalSignUpValidator = (req, res, next) => {
     req.check('password')
     .isLength({min:8})
     .withMessage("Password must contain at least 8 characters")
-    .matches(/\d/)
-    .withMessage('Password must contain a number')
+    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)
+    .withMessage('Password must contain a number and a special character')
     //check for errors:
     const errors = req.validationErrors()
     //map throught the errors:
@@ -94,6 +95,7 @@ exports.adminSignUpValidator = (req, res, next) => {
     })
     .withMessage("Name must contain at least 2 characters")
     //email is not null, valid and normalized with regular expression
+    req.check('adminEmail', "Email is required").notEmpty();
     req.check('adminEmail', 'Email must be between 3 to 32 characters')
     .matches(/.+@.+\..+/)
     .withMessage('Email must contain @')
@@ -103,10 +105,10 @@ exports.adminSignUpValidator = (req, res, next) => {
     //check for password
     req.check('password', "Password is required").notEmpty();
     req.check('password')
-    .isLength({min:5})
-    .withMessage("Password must contain at least 5 characters")
-    .matches(/\d/)
-    .withMessage('Password must contain a number')
+    .isLength({min:8})
+    .withMessage("Password must contain at least 8 characters")
+    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)
+    .withMessage('Password must contain a number and a special character')
     //check for errors:
     const errors = req.validationErrors()
     //map throught the errors:
@@ -137,3 +139,81 @@ exports.passwordResetValidator = (req, res, next) => {
     // proceed to next middleware or ...
     next();
 };
+
+exports.userSignInValidator = (req, res, next) => {
+    //email is not null, valid and normalized with regular expression
+    req.check('email', "Email is required").notEmpty();
+    req.check('email', 'Email must be between 3 to 32 characters')
+    .matches(/.+@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+        min: 4, max: 2000
+    })
+    //check for password
+    req.check('password', "Password is required").notEmpty();
+    req.check('password')
+    .isLength({min:8})
+    .withMessage("Password must contain at least 8 characters")
+    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)
+    .withMessage('Password must contain a number and a special character')
+    //check for errors:
+    const errors = req.validationErrors()
+    //map throught the errors:
+    if(errors){
+        const firstError = errors.map((error) => error.msg)[0]
+        return res.status(400).json({error: firstError})
+        }
+    next();
+}
+
+exports.hospitalSignInValidator = (req, res, next) => {
+    //email is not null, valid and normalized with regular expression
+    req.check('hospitalEmail', "Email is required").notEmpty();
+    req.check('hospitalEmail', 'Email must be between 3 to 32 characters')
+    .matches(/.+@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+        min: 4, max: 2000
+    })
+    //check for password
+    req.check('password', "Password is required").notEmpty();
+    req.check('password')
+    .isLength({min:8})
+    .withMessage("Password must contain at least 8 characters")
+    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)
+    .withMessage('Password must contain a number and a special character')
+    //check for errors:
+    const errors = req.validationErrors()
+    //map throught the errors:
+    if(errors){
+        const firstError = errors.map((error) => error.msg)[0]
+        return res.status(400).json({error: firstError})
+        }
+    next();
+}
+
+exports.adminSignInValidator = (req, res, next) => {
+    //email is not null, valid and normalized with regular expression
+    req.check('adminEmail', "Email is required").notEmpty();
+    req.check('adminEmail', 'Email must be between 3 to 32 characters')
+    .matches(/.+@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+        min: 4, max: 2000
+    })
+    //check for password
+    req.check('password', "Password is required").notEmpty();
+    req.check('password')
+    .isLength({min:8})
+    .withMessage("Password must contain at least 8 characters")
+    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/)
+    .withMessage('Password must contain a number and a special character')
+    //check for errors:
+    const errors = req.validationErrors()
+    //map throught the errors:
+    if(errors){
+        const firstError = errors.map((error) => error.msg)[0]
+        return res.status(400).json({error: firstError})
+        }
+    next();
+}
